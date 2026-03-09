08ab2c128adf0c73b03785b013d7fc2971a7e886
f.rosito@thomyris.com <f.rosito@thomyris.com>
2026-03-09 16:37:51 +0100

    feat: Handle delete by listener

--- database/migrations/2024_01_01_000000_create_passkeys_table.php
+++ database/migrations/2024_01_01_000000_create_passkeys_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,13 +19,10 @@ return new class extends Migration
             $table->text('public_key');
             $table->timestamps();
 
-            // Foreign key constraint
             $table->foreign('user_id')
                 ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
+                ->on('users');
 
-            // Indexes for performance
             $table->index('user_id');
             $table->index('credential_id');
         });
--- src/Traits/HasPasskeys.php
+++ src/Traits/HasPasskeys.php
@@ -8,6 +8,18 @@ use Xefi\LaravelPasskey\Models\Passkey;
 trait HasPasskeys
 {
     /**
+     * The "booting" method of the trait.
+     *
+     * @return void
+     */
+    protected static function bootHasPasskeys(): void
+    {
+        static::deleting(function ($user) {
+            $user->passkeys()->delete();
+        });
+    }
+
+    /**
      * Get all of the user's passkeys.
      *
      * @return HasMany
